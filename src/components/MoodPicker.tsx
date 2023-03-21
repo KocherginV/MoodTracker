95c2889ffb432eb877da95156f80777f03fc2781
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-03-21 12:01:23 +0100

    add mood picker ui

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { MoodOptionType } from '../types';
+import { theme } from '../theme';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
   { emoji: '😊', description: 'happy' },
@@ -10,19 +12,89 @@ const moodOptions = [
 ];
 
 export const MoodPicker: React.FC = () => {
+  const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
   return (
-    <View style={styles.moodOptions}>
-      {moodOptions.map(option => (
-        <Text key={option.emoji}>{option.emoji}</Text>
-      ))}
+    <View style={styles.moodContainer}>
+      <Text style={styles.headingText}>How are you right now?</Text>
+      <View style={styles.moodOptions}>
+        {moodOptions.map(option => (
+          <View>
+            <Pressable
+              onPress={() => setSelectedMood(option)}
+              key={option.emoji}
+              style={[
+                styles.moodItem,
+                option.emoji === selectedMood?.emoji
+                  ? styles.selectedMoodItem
+                  : undefined,
+              ]}>
+              <Text>{option.emoji}</Text>
+            </Pressable>
+            <Text style={styles.descriptionText}>
+              {option.emoji === selectedMood?.emoji
+                ? option.description
+                : undefined}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Pressable style={styles.chooseButton}>
+        <Text style={styles.buttonText}>Choose</Text>
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  moodContainer: {
+    borderColor: theme.colorPurple,
+    borderWidth: 2,
+    borderRadius: 10,
+    marginHorizontal: 10,
+    paddingVertical: 20,
+  },
+  headingText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+    letterSpacing: 1,
+    marginBottom: 15,
+  },
   moodOptions: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: 20,
+  },
+  moodItem: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginBottom: 5,
+  },
+  selectedMoodItem: {
+    borderWidth: 2,
+    backgroundColor: theme.colorPurple,
+    borderColor: theme.colorWhite,
+  },
+  descriptionText: {
+    color: theme.colorPurple,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 10,
+  },
+  chooseButton: {
+    backgroundColor: theme.colorPurple,
+    width: 150,
+    alignSelf: 'center',
+    padding: 10,
+    borderRadius: 20,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    textAlign: 'center',
+    fontWeight: 'bold',
   },
 });
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { MoodPicker } from '../components/MoodPicker';
 
 export const Home: React.FC = () => {
