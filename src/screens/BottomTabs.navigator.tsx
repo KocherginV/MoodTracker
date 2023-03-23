5d128c2df0aaf2fa183d2f31216dbac46e129871
Volodymyr Kochergin <volodymyrkochergin@gmail.com>
2023-03-23 12:24:05 +0100

    introduce svg icons for bottom nav

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -1,17 +1,49 @@
+/* eslint-disable react/no-unstable-nested-components */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import { Home } from './Home.screen';
 import { History } from './History.screen';
 import { Analytics } from './Analytics.screen';
+import { HomeIcon, HistoryIcon, AnalyticsIcon } from '../components/Icons';
+import { theme } from '../theme';
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="Home" component={Home} />
-      <BottomTabs.Screen name="History" component={History} />
-      <BottomTabs.Screen name="Analytics" component={Analytics} />
+    <BottomTabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: theme.colorBlue,
+        tabBarInactiveTintColor: theme.colorGrey,
+        tabBarShowLabel: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') {
+            return <HomeIcon color={color} size={size} />;
+          }
+          if (route.name === 'History') {
+            return <HistoryIcon color={color} size={size} />;
+          }
+          if (route.name === 'Analytics') {
+            return <AnalyticsIcon color={color} size={size} />;
+          }
+          return null;
+        },
+      })}>
+      <BottomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{ title: "Today's mood" }}
+      />
+      <BottomTabs.Screen
+        name="History"
+        component={History}
+        options={{ title: 'Past moods' }}
+      />
+      <BottomTabs.Screen
+        name="Analytics"
+        component={Analytics}
+        options={{ title: 'Fancy Charts' }}
+      />
     </BottomTabs.Navigator>
   );
 };
